f1392990d9f61673b6a59a4c2376131adc6fa34d
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-10 11:35:32 -0600

    feat: add relationship to customers and num_of_customers method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :supermarket
-  has_many :customers_items
-  has_many :customers, through: :customers_items
+  has_many :customer_items
+  has_many :customers, through: :customer_items
+
+  def num_of_customers
+    customers.size
+  end
+
 end
