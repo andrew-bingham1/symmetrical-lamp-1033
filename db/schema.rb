ad9fccb03aff1840a0ab99a44351d7928c93a83d
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-10 09:35:32 -0600

    fix: correct customer and customer items relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_10_151132) do
+ActiveRecord::Schema.define(version: 2023_04_10_153208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
