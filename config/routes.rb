460a31283f8dd8fac4eeef05c6d8e78f7971a097
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-10 11:10:01 -0600

    feat: create item index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
-get '/customers/:customer_id', to: 'customer#show'
+  get '/customers/:customer_id', to: 'customer#show'
 
-post '/customer_item', to: 'customer_item#create'
+  post '/customer_item', to: 'customer_item#create'
+
+  get '/items', to: 'item#index'
 end
