d70c1c7c2b67942cd27e079b6627c90d7169b1a4
Andrew Bingham <andrewgbingham@gmail.com>
2023-04-10 10:28:34 -0600

    feat: create show page and format

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'Customer Show Page' do
 
     @supermarket_1 = Supermarket.create!(name: "King Soopers", location: "Westminster, CO")
 
-    @item_1 = Item.create!(name: "Banana", price: 1, supermarket_id: @supermarket_1.id )
-    @item_2 = Item.create!(name: "Apple", price: 2, supermarket_id: @supermarket_1.id )
-    @item_3 = Item.create!(name: "Avocado", price: 3, supermarket_id: @supermarket_1.id )
+    @item_1 = Item.create!(name: "Banana", price: 1, supermarket: @supermarket_1)
+    @item_2 = Item.create!(name: "Apple", price: 2, supermarket: @supermarket_1)
+    @item_3 = Item.create!(name: "Avocado", price: 3, supermarket: @supermarket_1)
 
     CustomerItem.create!(customer: @customer_1, item: @item_1)
     CustomerItem.create!(customer: @customer_1, item: @item_2)
@@ -24,6 +24,34 @@ RSpec.describe 'Customer Show Page' do
     it "customer show page exists" do
       visit "/customers/#{@customer_1.id}"
     end
+
+    it "I see the customer's name" do
+      visit "/customers/#{@customer_1.id}"
+
+      expect(page).to have_content(@customer_1.name)
+    end
+
+    it "And I see its a list of its items including the item's name, price, and the name of the supermarket that it belongs to." do
+      visit "/customers/#{@customer_1.id}"
+
+      within("#Banana") do
+        expect(page).to have_content("Item Name: Banana")
+        expect(page).to have_content("Price: 1 dollar(s)")
+        expect(page).to have_content("Supermarket: King Soopers")
+      end
+      
+      within("#Apple") do
+        expect(page).to have_content("Item Name: Apple")
+        expect(page).to have_content("Price: 2 dollar(s)")
+        expect(page).to have_content("Supermarket: King Soopers")
+      end
+
+      within("#Avocado") do
+        expect(page).to have_content("Item Name: Avocado")
+        expect(page).to have_content("Price: 3 dollar(s)")
+        expect(page).to have_content("Supermarket: King Soopers")
+      end
+    end
   end
 end
 
@@ -33,4 +61,4 @@ end
 # When I visit a customer show page,
 # I see the customer's name,
 # And I see its a list of its items
-# including the item's name, price, and the name of the supermarket that 
+# including the item's name, price, and the name of the supermarket that it belongs to.
